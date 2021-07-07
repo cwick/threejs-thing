8bea59f4ae7d1cecda0bf88da54d6b05b916b3bb
Carmen Wick <carmenwick@gmail.com>
2021-07-07 02:36:15 -0700

    Finally, it works

--- src/InteractControls.js
+++ src/InteractControls.js
@@ -1,11 +1,21 @@
 import * as THREE from "/lib/three.js";
 
 export default class {
-  constructor({ controlStack, depthReader, renderer, camera }) {
+  constructor({ controlStack, depthReader, renderer, camera, scene }) {
     this.controlStack = controlStack;
     this.depthReader = depthReader;
     this.renderer = renderer;
     this.camera = camera;
+    this.scene = scene;
+    const geometry = new THREE.SphereGeometry(0.1, 4, 4);
+    const material = new THREE.MeshBasicMaterial({
+      fog: false,
+      toneMapped: false,
+      color: 0xff0000,
+    });
+    const sphere = new THREE.Mesh(geometry, material);
+    this.scene.add(sphere);
+    this.marker = sphere;
   }
 
   onMouseMove(movement) {
@@ -26,12 +36,15 @@ export default class {
       const depth = this.depthReader.readDepth(x, y);
       const size = this.renderer.getSize(new THREE.Vector2());
       const ndcX = (x / (size.x - 1)) * 2 - 1;
-      const ndcY = (y / (size.y - 1)) * 2 - 1;
+      const ndcY = ((size.y - y - 1) / size.y) * 2 - 1;
       const ndcDepth = depth * 2 - 1;
-      console.log("Pick", x / (size.x - 1), y / (size.y - 1), depth);
-      console.log(
-        new THREE.Vector3(ndcX, ndcY, ndcDepth).unproject(this.camera)
+      console.log("Pick", ndcX, ndcY, ndcDepth);
+      const worldPosition = new THREE.Vector3(ndcX, ndcY, ndcDepth).unproject(
+        this.camera
       );
+      console.log(worldPosition);
+
+      this.marker.position.copy(worldPosition);
     }
   }
 }
